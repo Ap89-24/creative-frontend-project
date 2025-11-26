ae19d6d72ec588ce651f16f00de13496ad825de4
Aman Patel <aman082199@gmail.com>
2025-11-26 14:47:40 +0530

    landing page

---
+++ src/components/home/HomeHeroText.jsx
@@ -0,0 +1,21 @@
+
+import Video from './Video'
+
+const HomeHeroText = () => {
+  return (
+    <div className='font-[font1] pt-3 text-center'>
+
+     <div className='text-[9vw] uppercase leading-31 flex items-center justify-center'>The spark for</div>
+     <div className='text-[9vw] uppercase leading-31 flex items-center justify-center'>all
+      <div className='h-[8vw] w-[15vw] rounded-full overflow-hidden p-1'>
+        <Video />
+      </div>
+        things
+     </div>
+     <div className='text-[9vw] uppercase leading-31 flex items-center justify-center'>creative</div>
+
+    </div>
+  )
+}
+
+export default HomeHeroText
