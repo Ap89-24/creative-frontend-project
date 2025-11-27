249ea7505f9d7477431388bf2df151732b6a9cfd
Aman Patel <aman082199@gmail.com>
2025-11-27 15:49:09 +0530

    agents page

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 
 const Agence = () => {
   return (
-    <div>
-      agence
+<div>
+
+  <div className='section-1'>
+    <div className='h-[20vw] w-54 rounded-3xl absolute top-50 left-107'>
+      <img className='rounded-3xl h-full w-full object-cover' src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
     </div>
+
+      <div className='font-[font2] relative z-20'>
+      <div className='mt-[30vw]'>
+        <h1 className='text-[20vw] leading-[16vw] text-center'>SEVEN7Y <br />
+         TWO</h1>
+      </div>
+
+      <div className='pl-[42%] mt-9'>
+        <p className='text-5xl'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
+      </div>
+    </div>
+</div>
+
+<div className='section-2 h-screen'>
+</div>
+
+</div>
+
   )
 }
 
