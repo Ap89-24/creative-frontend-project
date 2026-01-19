36b74082186b3798d0e8908a6c32044f84046b03
Aman Patel <aman082199@gmail.com>
2026-01-19 19:18:13 +0530

    work page css detailing

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,29 +3,24 @@
 const Projects = () => {
   return (
     <div className="p-5">
-      <div className='bg-red-200 pt-70'>
+      <div className='pt-70'>
         <h2 className='font-[font2] text-black text-[12vw] uppercase'>Work</h2>
       </div>
 
       <div className="-mt-14">
-        <div className="w-full h-[500px] flex gap-3 mb-3  bg-amber-300">
-          <div className="w-1/2 h-full bg-green-800">
-          <img className="h-full w-full object-cover" src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b" alt="" srcset="" />
+
+        <div className="w-full h-[500px] flex gap-3 mb-3 ">
+
+          <div className="w-1/2 h-full overflow-hidden hover:rounded-[30px] transition-all">
+          <img className="w-full h-full object-cover" src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b" alt="" srcset="" />
           </div>
-          <div className="w-1/2 h-full bg-blue-800">
-          <img lassName="h-full w-full object-cover" src="https://k72.ca/images/caseStudies/OKA/OKA_thumbnail.jpg?w=1280&h=960&s=c12c27c9db3c521e4c82a246a8d5c022" alt="" srcset="" />
+
+          <div className="w-1/2 h-full overflow-hidden hover:rounded-[30px] transition-all">
+          <img lassName="w-full h-full object-cover" src="https://k72.ca/images/caseStudies/OKA/OKA_thumbnail.jpg?w=1280&h=960&s=c12c27c9db3c521e4c82a246a8d5c022" alt="" srcset="" />
           </div>
+
         </div>
 
-        <div className="w-full h-[600px] flex gap-3 mb-3 bg-amber-300">
-          <div className="w-1/2 h-full bg-green-800"></div>
-          <div className="w-1/2 h-full bg-blue-800"></div>
-        </div>
-
-        <div className="w-full h-[600px] flex gap-3 mb-3  bg-amber-300">
-          <div className="w-1/2 h-full bg-green-800"></div>
-          <div className="w-1/2 h-full bg-blue-800"></div>
-        </div>
       </div>
     </div>
   )
